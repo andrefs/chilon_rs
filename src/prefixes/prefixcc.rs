987b76ca0edb306128a78cc1f329b03737d04b40
André Santos <andrefs@cpan.org>
2022-10-24 23:29:13 +0100

    wip

--- src/prefixes/prefixcc.rs
+++ src/prefixes/prefixcc.rs
@@ -5,6 +5,7 @@ use std::{
     fs::File,
     io::{BufReader, Read},
 };
+use trie_generic::Trie;
 use ureq;
 
 pub fn download() {
@@ -17,20 +18,30 @@ pub fn download() {
     parse(ttl);
 }
 
-pub fn parse(ttl: String) -> HashMap<String, String> {
+pub fn parse(ttl: String) -> Trie<String> {
     let mut parser = TurtleParser::new(ttl.as_ref(), None);
     parser
         .parse_all(&mut |_| Ok(()) as Result<(), TurtleError>)
         .unwrap();
     let pfs = parser.prefixes();
-    return pfs.to_owned();
+    let pref_hash = pfs.to_owned();
+
+    return build_namespace_trie(pref_hash);
 }
 
-pub fn load() -> HashMap<String, String> {
+pub fn load() -> Trie<String> {
     let pcc_path = "cache/prefix.cc.ttl";
     let file = File::open(pcc_path).unwrap();
     let mut buf_reader = BufReader::new(file);
     let mut s = String::new();
-    buf_reader.read_to_string(&mut s);
+    buf_reader.read_to_string(&mut s).unwrap();
     return parse(s);
+}
+
+pub fn build_namespace_trie(pref_hash: HashMap<String, String>) -> Trie<String> {
+    let mut t = Trie::new(None);
+    for (alias, namespace) in pref_hash.iter() {
+        t.add(namespace, Some(alias.to_owned()));
+    }
+    return t;
 }
