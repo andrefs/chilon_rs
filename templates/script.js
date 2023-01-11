0c5e0e848df523dc01cc55310f443a876e40897c
André Santos <andrefs@cpan.org>
2023-01-11 22:36:15 +0000

    wip

--- templates/script.js
+++ templates/script.js
@@ -21,6 +21,89 @@ const normalizeCounts = (elems) => {
 
 window.debFilterData = debounce(() => filterData());
 
+
+const update = () => {
+
+  let nodesParent = d3.select("svg g.nodes");
+  let edgesParent = d3.select("svg g.edges");
+
+  const nodeGroups = nodesParent
+    .selectAll("g")
+    .data(data.nodes)
+    .enter()
+    .append('g').attr('class', 'node-g')
+
+  var nodes = nodeGroups
+    .append("circle")
+    .attr("r", d => Math.ceil(d.normCount || 0))
+    .style("fill", (d, i) => '#B3D9CB')
+    .attr('data-id', (d, i) => d.id)
+    .attr("data-fill", (d, i) => '#B3D9CB')
+    .style("pointer-events", "visiblePainted")
+    .style('cursor', 'pointer')
+
+  var nodelabels = nodeGroups
+    .append("text")
+    .attr("x", d => d.x)
+    .attr("y", d => d.y)
+    .attr('font-size', d => Math.ceil(d.normCount / 2))
+    .attr('class', "nodelabel")
+    .text(d => d.name)
+    .attr('dominant-baseline', 'middle')
+    .style("pointer-events", "none")
+    .style('cursor', 'pointer')
+
+
+
+  const edgeGroups = edgesParent
+    .selectAll("g")
+    .data(data.links)
+    .enter()
+    .append('g').attr('class', 'edge-g')
+
+  var edgepaths = edgeGroups
+    .append('path')
+    //.attr('d',linkArc)
+    //.attr('d', d => {
+    //  console.log('XXXXXXXXXXXX d', d);
+    //  return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y
+    //})
+    .attr('class', 'edgepath')
+    .attr('fill-opacity', 0)
+    .attr('id', (d, i) => 'edgepath' + i)
+    .attr("stroke-width", d => Math.ceil(d.normCount / 10))
+    .attr('opacity', 0.3)
+    .attr("data-stroke-width", d => Math.ceil(d.normCount / 10))
+    .attr("data-source", d => d.source)
+    .attr("data-target", d => d.target)
+    .attr("data-label", d => d.label)
+    .style("stroke", (d) => colorHash[d.label])
+    //.style("stroke", '#b8b8b8')
+    .attr("data-stroke", (d) => colorHash[d.label])
+    //.style("pointer-events", "none");
+    .style("pointer-events", "visibleStroke")
+    .attr('marker-end', 'url(#triangle)')
+
+  var edgelabels = edgeGroups.append('text')
+    .style("pointer-events", "none")
+    .attr('class', 'edgelabel')
+    .attr('id', (d, i) => 'edgelabel' + i)
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'text-after-edge')
+    .attr('font-size', 15)
+    .attr('fill', '#999')
+
+  edgelabels.append('textPath')
+    .attr('xlink:href', (d, i) => '#edgepath' + i)
+    .style("pointer-events", "none")
+    .attr('startOffset', '50%')
+    .attr('text-anchor', 'middle')
+    .attr('text-anchor', 'middle')
+  //.text(d => d.label)
+
+}
+
+
 window.filterData = () => {
   const minNodes = d3.select('#minNodeOccursInput').node().value;
   const maxNodes = d3.select('#maxNodeOccursInput').node().value;
@@ -32,7 +115,7 @@ window.filterData = () => {
   window.data.nodes = originalData.nodes.filter(n => n.count >= minNodes && n.count <= maxNodes);
   window.data.links = originalData.links.filter(n => n.count >= minEdges && n.count <= maxEdges);
 
-  restartSimulation(window.simulation);
+  update(svg);
 };
 
 const setSliders = (nodes, links) => {
@@ -174,15 +257,20 @@ const calcLoop = (d) => {
 }
 
 function ticked() {
+  let edgepaths = d3.selectAll("svg g.edge-g path");
   edgepaths.attr('d', function(d) {
     const sId = d.source?.id ?? d.source;
     const tId = d.target?.id ?? d.target;
     return sId === tId ? calcLoop(d) : calcEdge(d);
   });
 
+  let nodeGroups = d3.selectAll("svg g.node-g");
+
+  let nodes = nodeGroups.selectAll("circle");
   nodes.attr("cx", d => d.x)
     .attr("cy", d => d.y)
 
+  let nodelabels = nodeGroups.selectAll("text");
   nodelabels.attr("x", d => d.x)
     .attr("y", d => d.y);
 
@@ -290,73 +378,6 @@ var svg = d3.select("svg")
 const width = svg.node().getBoundingClientRect().width;
 const height = svg.node().getBoundingClientRect().height;
 
-/**********
- * Edges  *
- **********/
-
-
-//// Per-type markers, as they don't inherit styles.
-//svg.append("svg:defs").selectAll("marker")
-//    .data(["suit", "licensing", "resolved"])
-//  .enter().append("svg:marker")
-//    .attr("id", 'triangle')
-//    .attr("viewBox", "0 -5 10 10")
-//    .attr("refX", 15)
-//    .attr("refY", -1.5)
-//    .attr('fill-opacity', 0.5)
-//    .attr("markerWidth", 6)
-//    .attr("markerHeight", 6)
-//    .attr("orient", "auto")
-//  .append("svg:path")
-//    .attr("d", "M0,-5L10,0L0,5");
-
-const edgeGroups = svg.append('g')
-  .attr('class', 'edges')
-  .selectAll("g")
-  .data(data.links)
-  .enter()
-  .append('g').attr('class', 'edge-g')
-
-var edgepaths = edgeGroups
-  .append('path')
-  //.attr('d',linkArc)
-  //.attr('d', d => {
-  //  console.log('XXXXXXXXXXXX d', d);
-  //  return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y
-  //})
-  .attr('class', 'edgepath')
-  .attr('fill-opacity', 0)
-  .attr('id', (d, i) => 'edgepath' + i)
-  .attr("stroke-width", d => Math.ceil(d.normCount / 10))
-  .attr('opacity', 0.3)
-  .attr("data-stroke-width", d => Math.ceil(d.normCount / 10))
-  .attr("data-source", d => d.source)
-  .attr("data-target", d => d.target)
-  .attr("data-label", d => d.label)
-  .style("stroke", (d) => colorHash[d.label])
-  //.style("stroke", '#b8b8b8')
-  .attr("data-stroke", (d) => colorHash[d.label])
-  //.style("pointer-events", "none");
-  .style("pointer-events", "visibleStroke")
-  .attr('marker-end', 'url(#triangle)')
-
-var edgelabels = edgeGroups.append('text')
-  .style("pointer-events", "none")
-  .attr('class', 'edgelabel')
-  .attr('id', (d, i) => 'edgelabel' + i)
-  .attr('text-anchor', 'middle')
-  .attr('dominant-baseline', 'text-after-edge')
-  .attr('font-size', 15)
-  .attr('fill', '#999')
-
-edgelabels.append('textPath')
-  .attr('xlink:href', (d, i) => '#edgepath' + i)
-  .style("pointer-events", "none")
-  .attr('startOffset', '50%')
-  .attr('text-anchor', 'middle')
-  .attr('text-anchor', 'middle')
-//.text(d => d.label)
-
 
 /**********
  * Simulation  *
@@ -397,38 +418,13 @@ function initSimulation(simulation, data) {
 
 const nodeDistance = 300;
 window.simulation = d3.forceSimulation();
+
+
+svg.append('g').attr('class', 'edges')
+svg.append('g').attr('class', 'nodes');
+update(svg);
+
 initSimulation(window.simulation, window.data);
-
-/**********
- * Nodes  *
- **********/
-
-const nodeGroups = svg.append('g')
-  .attr('class', 'nodes')
-  .selectAll("g")
-  .data(data.nodes)
-  .enter()
-  .append('g').attr('class', 'node-g')
-
-var nodes = nodeGroups
-  .append("circle")
-  .attr("r", d => Math.ceil(d.normCount || 0))
-  .style("fill", (d, i) => '#B3D9CB')
-  .attr('data-id', (d, i) => d.id)
-  .attr("data-fill", (d, i) => '#B3D9CB')
-  .style("pointer-events", "visiblePainted")
-  .style('cursor', 'pointer')
-
-var nodelabels = nodeGroups
-  .append("text")
-  .attr("x", d => d.x)
-  .attr("y", d => d.y)
-  .attr('font-size', d => Math.ceil(d.normCount / 2))
-  .attr('class', "nodelabel")
-  .text(d => d.name)
-  .attr('dominant-baseline', 'middle')
-  .style("pointer-events", "none")
-  .style('cursor', 'pointer')
 
 
 /**********
