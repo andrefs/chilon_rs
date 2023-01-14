64c5d950b3e2cdc3a75c2233ed1e7db2f3d6511d
André Santos <andrefs@cpan.org>
2023-01-14 00:11:07 +0000

    move sliders and update sliders output

--- chilon-viz/src/events/sliders.ts
+++ chilon-viz/src/events/sliders.ts
@@ -1,1 +1,53 @@
-export { }
+
+const handleMinSlider = (
+  minSliderEl: HTMLInputElement | null,
+  sliderOutput: HTMLOutputElement | null,
+  maxSliderEl: HTMLInputElement | null
+) => {
+  if (minSliderEl?.value) {
+    let n = Math.abs(Number(minSliderEl.value))
+    sliderOutput!.textContent = n.toString();
+
+    if (maxSliderEl?.value && Number(maxSliderEl.value) < Number(minSliderEl.value)) {
+      maxSliderEl.value = minSliderEl.value;
+    }
+  }
+};
+
+const handleMaxSlider = (maxSliderEl: HTMLInputElement | null, sliderOutput: HTMLOutputElement | null, minSliderEl: HTMLInputElement | null) => {
+  if (maxSliderEl?.value) {
+    let n = Math.abs(Number(maxSliderEl.value))
+    sliderOutput!.textContent = n.toString();
+
+    if (minSliderEl?.value && Number(minSliderEl.value) > Number(maxSliderEl?.value)) {
+      minSliderEl.value = maxSliderEl?.value;
+    }
+  }
+};
+
+
+
+export const initSliderListeners = () => {
+  const minNodeOccursIn = document.querySelector<HTMLInputElement>('#minNodeOccursInput');
+  const minNodeOccursOut = document.querySelector<HTMLOutputElement>('#minNodeOccurs');
+
+  const maxNodeOccursIn = document.querySelector<HTMLInputElement>('#maxNodeOccursInput');
+  const maxNodeOccursOut = document.querySelector<HTMLOutputElement>('#maxNodeOccurs');
+
+  const minEdgeOccursIn = document.querySelector<HTMLInputElement>('#minEdgeOccursInput');
+  const minEdgeOccursOut = document.querySelector<HTMLOutputElement>('#minEdgeOccurs');
+
+  const maxEdgeOccursIn = document.querySelector<HTMLInputElement>('#maxEdgeOccursInput');
+  const maxEdgeOccursOut = document.querySelector<HTMLOutputElement>('#maxEdgeOccurs');
+
+  handleMinSlider(minNodeOccursIn, minNodeOccursOut, maxNodeOccursIn);
+  handleMaxSlider(maxNodeOccursIn, maxNodeOccursOut, minNodeOccursIn);
+  handleMinSlider(minEdgeOccursIn, minEdgeOccursOut, maxEdgeOccursIn);
+  handleMaxSlider(maxEdgeOccursIn, maxEdgeOccursOut, minEdgeOccursIn);
+
+  minNodeOccursIn!.addEventListener('input', () => handleMinSlider(minNodeOccursIn, minNodeOccursOut, maxNodeOccursIn));
+  maxNodeOccursIn!.addEventListener('input', () => handleMaxSlider(maxNodeOccursIn, maxNodeOccursOut, minNodeOccursIn));
+  minEdgeOccursIn!.addEventListener('input', () => handleMinSlider(minEdgeOccursIn, minEdgeOccursOut, maxEdgeOccursIn));
+  maxEdgeOccursIn!.addEventListener('input', () => handleMaxSlider(maxEdgeOccursIn, maxEdgeOccursOut, minEdgeOccursIn));
+};
+
--- chilon-viz/src/main.ts
+++ chilon-viz/src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
 import { rawData } from './data';
 import { select } from 'd3-selection';
+import { initSliderListeners } from './events/sliders';
+
+
+initSliderListeners();
 
 let svg = select("#app svg");
 svg.append('g').attr('class', 'edges')
