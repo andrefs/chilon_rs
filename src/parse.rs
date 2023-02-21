c382ab319cfe00423004aaed5c4774b8195513f6
André Santos <andrefs@cpan.org>
2023-02-21 19:43:23 +0000

    add nquads parser

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,5 @@
-use rio_api::parser::TriplesParser;
-use rio_turtle::{NTriplesParser, TurtleError, TurtleParser};
+use rio_api::parser::{QuadsParser, TriplesParser};
+use rio_turtle::{NQuadsParser, NTriplesParser, TurtleError, TurtleParser};
 
 use crate::extract::{extract, ReaderWrapper};
 use std::{
@@ -11,15 +11,21 @@ pub struct NTWrapper {
     prefixes: HashMap<String, String>,
     parser: NTriplesParser<ReaderWrapper>,
 }
+pub struct NQWrapper {
+    prefixes: HashMap<String, String>,
+    parser: NQuadsParser<ReaderWrapper>,
+}
 pub enum ParserWrapper {
     Turtle(TurtleParser<ReaderWrapper>),
     NTriples(NTWrapper),
+    NQuads(NQWrapper),
 }
 
 impl TriplesParser for ParserWrapper {
     type Error = TurtleError;
     fn is_end(&self) -> bool {
         match &self {
+            &ParserWrapper::NQuads(w) => w.parser.is_end(),
             &ParserWrapper::NTriples(w) => w.parser.is_end(),
             &ParserWrapper::Turtle(p) => p.is_end(),
         }
@@ -31,6 +37,14 @@ impl TriplesParser for ParserWrapper {
     ) -> Result<(), E> {
         match self {
             ParserWrapper::NTriples(p) => p.parser.parse_step(on_triple),
+            ParserWrapper::NQuads(p) => p.parser.parse_step(&mut |q| {
+                let t = rio_api::model::Triple {
+                    subject: q.subject,
+                    predicate: q.predicate,
+                    object: q.object,
+                };
+                on_triple(t)
+            }),
             ParserWrapper::Turtle(p) => p.parse_step(on_triple),
         }
     }
@@ -41,6 +55,7 @@ impl ParserWrapper {
         match self {
             ParserWrapper::Turtle(p) => p.prefixes(),
             ParserWrapper::NTriples(w) => &w.prefixes,
+            ParserWrapper::NQuads(w) => &w.prefixes,
         }
     }
 }
@@ -50,14 +65,22 @@ pub fn parse(path: &PathBuf) -> ParserWrapper {
     let path_stem = Path::new(file_stem);
     let ext = path_stem.extension();
 
-    if ext.is_some() && ext.unwrap() == "nt" {
-        let parser = NTriplesParser::new(stream);
-        return ParserWrapper::NTriples(NTWrapper {
-            prefixes: Default::default(),
-            parser,
-        });
-    } else {
-        let parser = TurtleParser::new(stream, None);
-        return ParserWrapper::Turtle(parser);
+    if let Some(ext) = ext {
+        if ext == "nt" {
+            let parser = NTriplesParser::new(stream);
+            return ParserWrapper::NTriples(NTWrapper {
+                prefixes: Default::default(),
+                parser,
+            });
+        }
+        if ext == "nq" {
+            let parser = NQuadsParser::new(stream);
+            return ParserWrapper::NQuads(NQWrapper {
+                prefixes: Default::default(),
+                parser,
+            });
+        }
     }
+    let parser = TurtleParser::new(stream, None);
+    return ParserWrapper::Turtle(parser);
 }
